018294c39d406c62f203bb1f7ae53c7720bf4766
Serghei Filippov <blastowind@gmail.com>
2016-07-17 14:29:47 -0400

    Fixes brew install

--- bash/install_brew.bash
+++ bash/install_brew.bash
@@ -2,15 +2,15 @@
 
 which -s brew
 if [[ $? == 1 ]]; then
-    BREW_INSTALL=$(curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/master/install')
+    BREW_INSTALL="curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/master/install'"
 
     # Install Homebrew
     EXPECT_MD5=d21086ee75f3f1ae20577e5e428f79de
-    ACTUAL_MD5=$(md5 -q <(echo ${BREW_INSTALL}))
+    ACTUAL_MD5=$(md5 -q <(${BREW_INSTALL}))
 
     # TODO: Checksum is currently disabled
     if diff <(echo ${ACTUAL_MD5}) <(echo ${EXPECT_MD5}) > /dev/null || true; then
-        ruby -e "<(echo ${BREW_INSTALL})"
+        ruby -e "<(${BREW_INSTALL})"
     else
         echo "md5 checksum for brew invalid"
         echo "Expected: ${EXPECT_MD5}"
