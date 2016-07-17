e486537dca8ec89536dd794d329189c24933110a
Serghei Filippov <blastowind@gmail.com>
2016-07-17 13:49:40 -0400

    Final Updates for v1.1

---
+++ bash/install_brew.bash
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+which -s brew
+if [[ $? == 0 ]]; then
+    BREW_INSTALL="curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install"
+
+    # Install Homebrew
+    EXPECT_MD5=90277433f4cdd6034fb14173163b374f
+    ACTUAL_MD5=$(md5 -q <(${BREW_INSTALL}))
+
+    if diff <(echo ${ACTUAL_MD5}) <(echo ${EXPECT_MD5}) > /dev/null; then
+        ruby -e "<(${BREW_INSTALL})"
+    else
+        echo "md5 checksum for brew invalid"
+        echo "Expected: ${EXPECT_MD5}"
+        echo "Actual:   ${ACTUAL_MD5}"
+        exit 1
+    fi
+fi
--- cpi.bash
+++ cpi.bash
@@ -2,6 +2,23 @@
 
 CPI=${CPI:-cp -i}
 
-if [ ! "$1" -ef "$2" ]; then
-	${CPI} $1 $2 || true
+
+# Args
+from=$1
+to=$2
+
+# If the second arg is a directory, we need to add the file name
+base=$(basename "$from")
+if [ -d "$to" ]; then
+	to=$to/$base
+fi
+
+# The file can either point to the same file (if you're linking)
+if [[ "${CPI}" =~ .*"ln".* ]]; then
+	if [ ! "$from" -ef "$to" ]; then
+		${CPI} $from $to || true
+	fi
+# Or it needs to have the right contents
+elif [ $(cmp --silent "$from" "$to" && echo 1 || echo 0) -eq "0" ]; then
+	${CPI} $from $to || true
 fi
