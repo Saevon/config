56a5109de1993b0380260b6315097fe816894ab5
Serghei Filippov <blastowind@gmail.com>
2016-07-21 22:04:15 -0400

    Fixes brew installer

--- bash/install_brew.bash
+++ bash/install_brew.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-which -s brew
-if [[ $? == 1 ]]; then
+which brew 2>&1 > /dev/null
+if [[ $? != 0 ]]; then
     ruby -e "$(curl -fsSL 'https://raw.githubusercontent.com/Homebrew/install/master/install')"
 fi
