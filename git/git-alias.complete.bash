c7e7948d742d461b125e5bd65dc16bd2d76cdd1f
Serghei Filippov <blastowind@gmail.com>
2016-08-05 10:53:34 -0400

    Adds completion for some git aliases

---
+++ git/git-alias.complete.bash
@@ -0,0 +1,39 @@
+
+_git_branches_remote() {
+	local remote="$1"
+	git branch -a | gawk -v remote=${remote} 'match($0, / *[*]? *(remotes\/([^/]+)\/)?([^ ]+)/, cap) { if (cap[2] == remote && index(cap[3], "HEAD") == 0) { print cap[3] }}' | sort | uniq
+}
+
+_git_branches_all() {
+	git branch -a | gawk 'match($0, / *[*]? *(remotes\/([^/]+)\/)?([^ ]+)/, cap) { if (index(cap[3], "HEAD") == 0) { print cap[3] }}' | sort | uniq
+}
+
+
+_git_push_to() {
+	local cur prev
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+	if [ ${COMP_CWORD} == 2 ]; then
+		_git_ls_remote
+	elif [ ${COMP_CWORD} == 3 ]; then
+		local branches=$(_git_branches_all $1)
+		COMPREPLY=( $(compgen -W "$branches" -- ${cur}) )
+	fi
+}
+
+_git_del_branch() {
+	local cur prev
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+	if [ ${COMP_CWORD} == 2 ]; then
+		_git_ls_remote
+	elif [ ${COMP_CWORD} == 3 ]; then
+		local branches=$(_git_branches_remote $1)
+		COMPREPLY=( $(compgen -W "$branches" -- ${cur}) )
+	fi
+}
+
