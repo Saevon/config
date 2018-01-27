b6bc573eb4d8f012a7f80f8ce9fb111d21cf1a45
Serghei Filippov <serghei.filippov@gmail.com>
2018-01-26 22:29:33 -0800

    Git adds more completions

--- git/git-alias.complete.bash
+++ git/git-alias.complete.bash
@@ -22,6 +22,21 @@ _git_push_to() {
 	fi
 }
 
+_git_push_commit() {
+   local cur prev
+   COMPREPLY=()
+   cur="${COMP_WORDS[COMP_CWORD]}"
+   prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+   if [ ${COMP_CWORD} == 2 ]; then
+       _git_ls_remote
+   elif [ ${COMP_CWORD} == 4 ]; then
+       local branches=$(_git_branches_all $1)
+       COMPREPLY=( $(compgen -W "$branches" -- ${cur}) )
+   fi
+}
+
+
 _git_del_branch() {
 	local cur prev
 	COMPREPLY=()
@@ -46,6 +61,20 @@ _git_fetchff() {
 		_git_ls_remote
 	elif [ ${COMP_CWORD} == 3 ]; then
 		local branches=$(_git_branches_all $1)
+        local branches=$(_git_branches_remote $prev)
+        COMPREPLY=( $(compgen -W "$branches" -- ${cur}) )
+   fi
+}
+
+_git_kill_branch() {
+    local cur prev
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    if [ ${COMP_CWORD} == 2 ]; then
+        _git_ls_remote
+    elif [ ${COMP_CWORD} == 3 ]; then
+        local branches=$(_git_branches_remote $prev)
 		COMPREPLY=( $(compgen -W "$branches" -- ${cur}) )
 	fi
 }
