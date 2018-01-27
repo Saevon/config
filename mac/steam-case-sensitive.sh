e0f88adea5f0b6e6a1c5aacc4e3513a18250326a
Serghei Filippov <serghei.filippov@gmail.com>
2018-01-26 16:32:33 -0800

    Adds a fix for case-sensitive Mac & Steam

---
+++ mac/steam-case-sensitive.sh
@@ -0,0 +1,29 @@
+cd ~/Library/Application\ Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/public;
+ls *.res | while read line; do
+	file=$(echo $line | tr '[:upper:]' '[:lower:]');
+	mv $line $file;
+done
+
+cd ~/Library/Application\ Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/steam/cached/;
+ls *.res | while read line; do
+	file=$(echo $line | tr '[:upper:]' '[:lower:]');
+	mv $line $file;
+done
+
+cd ~/Library/Application\ Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/friends;
+ls *.res | while read line; do
+	file=$(echo $line | tr '[:upper:]' '[:lower:]');
+	mv $line $file;
+done
+
+cd ~/Library/Application\ Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/graphics;
+ls *.tga | while read line; do
+	file=$(echo $line | tr '[:upper:]' '[:lower:]');
+	mv $line $file;
+done
+
+cd ~/Library/Application\ Support/Steam/Steam.AppBundle/Steam/Contents/MacOS/servers;
+ls *.tga | while read line; do
+	file=$(echo $line | tr '[:upper:]' '[:lower:]');
+	mv $line $file;
+done
