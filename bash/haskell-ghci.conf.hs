d0117f8770b7d879c7b13fe69edc72152877e741
Serghei Filippov <serghei.filippov@gmail.com>
2019-03-27 13:33:08 -0700

    GHCI: adds Partial signatures

---
+++ bash/haskell-ghci.conf.hs
@@ -0,0 +1,15 @@
+-- Enables multiline input
+:set +m
+
+-- Allows you to use _ as a wildcard in type signatures
+:set -XPartialTypeSignatures
+
+-- Other common extensionns
+-- :set -XGADTSyntax
+-- :set -XGeneralizedNewtypeDeriving
+-- :set -XInstanceSigs
+-- :set -XLambdaCase
+-- :set -XPartialTypeSignatures
+-- :set -XScopedTypeVariables
+-- :set -XTypeApplications
+-- :set -XOverloadedStrings
